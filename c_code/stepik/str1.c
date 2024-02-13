12539919ea35cd09b3799a1364eef5abf9f5c91a
hudhuud <118181770+hudhuud@users.noreply.github.com>
2024-02-13 13:10:58 +0300

    Update str1.c

---
+++ c_code/stepik/str1.c
@@ -0,0 +1,19 @@
+Напишите программу ввода двух строк (каждая вводится с новой строки) и их объединения в одну строку через пробел. Результат выведите на экран.
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100];
+
+    fgets(str1, sizeof(str1), stdin);
+
+    fgets(str2, sizeof(str2), stdin);
+
+    str1[strcspn(str1, "\n")] = '\0';
+    str2[strcspn(str2, "\n")] = '\0';
+
+    printf("%s %s", str1, str2);
+
+    return 0;
+}
