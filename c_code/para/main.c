9609f9a4450e50841927ca67fdd046b6d0c50a31
hudhuud <118181770+hudhuud@users.noreply.github.com>
2024-04-17 15:45:38 +0300

    Update and rename 1.c to main.c

--- c_code/para/main.c
+++ c_code/para/main.c
@@ -1,42 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "sort.h"
 
 #define max_len 100
-
-void sort(char ***res, int *n, int *siz, char *buf) {
-    //есть ли место в res
-    if (*n >= *siz) {
-        //увеличение вдвое
-        *siz *= 2;
-        *res = realloc(*res, (*siz) * sizeof(char *));
-        if (*res == NULL) {
-            printf("ошибка при выделении памяти 1\n");
-            exit(1);
-        }
-    }
-
-    //копирование строки
-    (*res)[*n] = strdup(buf);
-    (*n)++;
-}
 
 int main() {
     FILE *file;
     char **res = NULL;
     char buf[max_len];
-    int n = 0; //строки в res
-    int siz = 1; //начальный размер
+    int n = 0; // строки в res
+    int siz = 1; // начальный размер
     int i;
-    
-    //для qsort
-    int compare(const void *a, const void *b) {
-    const char *str1 = *(const char **)a;
-    const char *str2 = *(const char **)b;
-    return strcmp(str1, str2);
-    }
-    
-    //выделение памяти для res
+
     res = (char **)malloc(siz * sizeof(char *));
     if (res == NULL) {
         printf("ошибка при выделении памяти 2\n");
@@ -50,7 +26,6 @@ int main() {
     }
 
     while (fgets(buf, max_len, file) != NULL) {
-
         buf[strcspn(buf, "\n")] = '\0';
         sort(&res, &n, &siz, buf);
     }
